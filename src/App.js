bdd4b440add67551612824f6f79dbd897dddd4cc
Ervin Khaliliaev <ervinhalilyaev123@gmail.com>
2020-10-09 20:27:20 +0300

    init

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import Link from './components/Link'
+import './App.scss';
+
+function App() {
+  return (
+    <>
+   <Link></Link>
+   <a></a>
+   </>
+  );
+}
+
+export default App;
---
+++ src/components/Link.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Link = () => {
+    return (
+        <a className="jhfhjf"></a>
+    )
+}
+
+export default Link;
